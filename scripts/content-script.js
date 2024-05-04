acf57ac9f645aff5ea34f99bb8bfcc39d4d9f85d
AndreiLeIttu <andrei.ionita2003@gmail.com>
2024-05-04 20:39:47 +0200

    Added constraints for yellow, green and unused letters.

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -5,32 +5,53 @@ function solve() {
         let str = "#board > game-row:nth-child(" + i + ")"
         let cur_child = document.querySelector("body > game-app").shadowRoot.querySelector(str)
         if (cur_child.getAttribute("letters").length === 0) {
-            console.log(i)
+            console.log("Empty row num: " + i)
             field = cur_child
             field_num = i
             break
         }
     }
+    let available = [];
+    let mustBePresent = {}
+    for (let i = 0; i < 5; i++) {
+        available.push(new Set("abcdefghijklmnopqrstuvwxyz"));
+    }
+    let yellow_count = new Array(26).fill(0);
 
     //go through previous rows and mark wrong letters, as well as correct and misplaced letters.
     for (let i=1;i<field_num;i++) {
         let row_str = "#board > game-row:nth-child(" + i + ")"
+        let cur_yellow_count = new Array(26).fill(0);
         for (let j=1;j<=5;j++) {
             let column_str = "div > game-tile:nth-child(" + j + ")"
             let elem = document.querySelector("body > game-app").shadowRoot.querySelector(row_str).shadowRoot.querySelector(column_str)
             let letter_status = elem.getAttribute("evaluation")
+            let letter = elem.shadowRoot.querySelector("div").innerText.toLowerCase()
             if (letter_status === "correct") {
                 //column j is correct
+                available[j-1].clear()
+                available[j-1].add(letter)
+                cur_yellow_count[letter.charCodeAt(0) - 'a'.charCodeAt(0)]++
             }
             else if (letter_status === "present") {
                 //letter on column j is yellow, so misplaced
+                available[j-1].delete(letter)
+                console.log(letter.charCodeAt(0) - 'a'.charCodeAt(0))
+                cur_yellow_count[letter.charCodeAt(0) - 'a'.charCodeAt(0)]++
             }
             else {
                 //letter should not be used in future tries
+                for (let k=0;k<5;k++)
+                    available[k].delete(letter)
             }
         }
+        for (let j=0;j<26;j++)
+            yellow_count[j]=Math.max(yellow_count[j], cur_yellow_count[j])
     }
+    console.log(available)
+    console.log(yellow_count)
 
+    //look through the whole list of words and choose word with max 'score' that also satisfies the constraints.
 }
 
 solve()
